f58cac7eedd419fa53a642524e9344e0100fe49e
jiyouliang <jeyouliang@gmail.com>
2019-09-10 16:22:26 +0800

    复杂布局

--- navigations/AppNavigation.js
+++ navigations/AppNavigation.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, StyleSheet} from 'react-native';
 import {createAppContainer, createStackNavigator} from 'react-navigation'; // Version can be specified in package.json
-import HomePage from './HomePage';
+// import HomePage from './HomePage';
 import PropsPage from './PropsPage';
 import StatePage from './StatePage';
 import WidthHeightPage from './WidthHeightPage';
 import FlexWidthHeightPage from './FlexWidthHeightPage';
+import MixedLayoutPage from './MixedLayoutPage';
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <View style={{flexDirection: 'column', flex: 1, alignItems: 'stretch'}}>
+                <Text style={styles.itemStyle}
+                      onPress={() => this.props.navigation.navigate('PropsPage')}>Props属性</Text>
+                <Text style={styles.itemStyle}
+                      onPress={() => this.props.navigation.navigate('StatePage')}>State状态</Text>
+                <Text style={styles.itemStyle}
+                      onPress={() => this.props.navigation.navigate('WidthHeightPage')}>宽度和高度</Text>
+                <Text style={styles.itemStyle}
+                      onPress={() => this.props.navigation.navigate('FlexWidthHeightPage')}>flex宽度和高度</Text>
+                <Text style={styles.itemStyle}
+                      onPress={() => this.props.navigation.navigate('MixedLayoutPage')}>ReactNative实现复杂布局</Text>
+
+            </View>
+        );
+    }
+}
 
 
 const appStackNavigator = createStackNavigator(
@@ -25,6 +46,9 @@ const appStackNavigator = createStackNavigator(
         FlexWidthHeightPage: {
             screen: FlexWidthHeightPage
         },
+        MixedLayoutPage: {
+            screen: MixedLayoutPage
+        },
     },
     {
         initialRouteName: 'Home',
@@ -33,8 +57,33 @@ const appStackNavigator = createStackNavigator(
 
 export const AppContainer = createAppContainer(appStackNavigator);
 
-// export default class App extends React.Component {
-//     render() {
-//         return <AppContainer />;
-//     }
-// }
+const styles = StyleSheet.create({
+
+    itemStyle: {
+        marginTop: 10,
+        height: 30,
+        backgroundColor: '#c3c3c3',
+        lineHeight: 30,
+        textAlign: 'center',
+        fontWeight: 'bold'
+
+    },
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
--- navigations/HomePage.js
+++ navigations/HomePage.js
@@ -1,52 +1,55 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Button} from 'react-native-elements';
-
-
-
-export default class HomePage extends React.Component {
-    render() {
-        return (
-            <View style={{flexDirection: 'column', flex: 1, alignItems: 'stretch'}}>
-                <Text style={styles.itemStyle}
-                      onPress={() => this.props.navigation.navigate('PropsPage')}>Props属性</Text>
-                <Text style={styles.itemStyle}
-                      onPress={() => this.props.navigation.navigate('StatePage')}>State状态</Text>
-                <Text style={styles.itemStyle}
-                      onPress={() => this.props.navigation.navigate('WidthHeightPage')}>宽度和高度</Text>
-                <Text style={styles.itemStyle}
-                      onPress={() => this.props.navigation.navigate('FlexWidthHeightPage')}>flex宽度和高度</Text>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-
-    itemStyle: {
-        marginTop: 10,
-        height: 30,
-        backgroundColor: '#c3c3c3',
-        lineHeight: 30,
-        textAlign: 'center',
-        fontWeight: 'bold'
-
-    },
-
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+// import {Button} from 'react-native-elements';
+//
+//
+//
+// export default class HomePage extends React.Component {
+//     render() {
+//         return (
+//             <View style={{flexDirection: 'column', flex: 1, alignItems: 'stretch'}}>
+//                 <Text style={styles.itemStyle}
+//                       onPress={() => this.props.navigation.navigate('PropsPage')}>Props属性</Text>
+//                 <Text style={styles.itemStyle}
+//                       onPress={() => this.props.navigation.navigate('StatePage')}>State状态</Text>
+//                 <Text style={styles.itemStyle}
+//                       onPress={() => this.props.navigation.navigate('WidthHeightPage')}>宽度和高度</Text>
+//                 <Text style={styles.itemStyle}
+//                       onPress={() => this.props.navigation.navigate('FlexWidthHeightPage')}>flex宽度和高度</Text>
+//                 <Text style={styles.itemStyle}
+//                       onPress={() => this.props.navigation.navigate('MixedLayoutPage')}>ReactNative实现复杂布局</Text>
+//
+//             </View>
+//         );
+//     }
+// }
+//
+// const styles = StyleSheet.create({
+//
+//     itemStyle: {
+//         marginTop: 10,
+//         height: 30,
+//         backgroundColor: '#c3c3c3',
+//         lineHeight: 30,
+//         textAlign: 'center',
+//         fontWeight: 'bold'
+//
+//     },
+//
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#F5FCFF',
+//     },
+//     welcome: {
+//         fontSize: 20,
+//         textAlign: 'center',
+//         margin: 10,
+//     },
+//     instructions: {
+//         textAlign: 'center',
+//         color: '#333333',
+//         marginBottom: 5,
+//     },
+// });
