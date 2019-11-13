abdd8cd8546073d48ef71de147096825d2dbb7ff
jiyouliang <jeyouliang@gmail.com>
2019-11-13 16:34:38 +0800

    重构简单redux

--- navigations/redux/SimpleReduxPage.tsx
+++ navigations/redux/SimpleReduxPage.tsx
@@ -1,58 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { NetworkPagesContainer } from "../NetworkPages";
+import { NavigationScreenProps } from "react-navigation";
+import { NavigationStackScreenOptions } from "react-navigation-stack/src/types";
+import AppCounter from "./AppCounter";
 
 export default class SimpleReduxPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0,
+  public static navigationOptions = (
+    props: NavigationScreenProps
+  ): NavigationStackScreenOptions => {
+    return {
+      title: "redux简单实现"
     };
-  }
-
-  //增加
-  private increase = () => {
-    this.setState({
-      value: this.state.value + 1,
-    });
-  }
-
-  private decrease = () => {
-    this.setState({
-      value: this.state.value - 1,
-    });
-  }
+  };
+ 
 
   public render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.decrease}>
-          <Text style={styles.title}>-</Text>
-        </TouchableOpacity>
-        <Text style={styles.value}>{this.state.value}</Text>
-        <TouchableOpacity onPress={this.increase}>
-          <Text style={styles.title}>+</Text>
-        </TouchableOpacity>
-      </View>
+      <AppCounter />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  title: {
-    fontSize: 30,
-    color: "red",
-    paddingLeft: 10,
-    paddingRight: 10
-  },
-  value: {
-    fontSize: 30,
-    color: "#080808"
-  }
-});
