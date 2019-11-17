257fede70ea2fbd168f41a076d47ad113f8052c8
jiyouliang <jeyouliang@gmail.com>
2019-11-17 19:47:45 +0800

    简单Redux

--- navigations/redux/AppCounter.tsx
+++ navigations/redux/AppCounter.tsx
@@ -1,68 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import {createStore} from 'redux';
-import { Provider } from 'react-redux';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity
+} from "react-native";
+import { connect } from 'react-redux'
+class CounterApp extends Component {
 
-/**
- * Store - holds our state - THERE IS ONLY ONE STATE 
- * Action - State can be modified using actions - SIMPLE OBJECTS 
- * Dispatcher - Action needs to be sent by someone - known as dispatching an action
- * Reducer - receives the action and modifies the state to give us a new state 
- *  - pure functions 
- *  - only mandatory argument is the 'type' 
- * Subscriber - listens for state change to update the ui  
- */
 
- 
-
-export default class AppCounter extends React.Component {
-  state = {
-    value: 0
-  };
-
-  //增加
-  private increase = () => {
-    this.setState({
-      value: this.state.value + 1
-    });
-  };
-
-  private decrease = () => {
-    this.setState({
-      value: this.state.value - 1
-    });
-  };
-
-  public render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.decrease}>
-          <Text style={styles.title}>-</Text>
-        </TouchableOpacity>
-        <Text style={styles.value}>{this.state.value}</Text>
-        <TouchableOpacity onPress={this.increase}>
-          <Text style={styles.title}>+</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{ flexDirection: 'row', width: 200, justifyContent: 'space-around' }}>
+                    <TouchableOpacity onPress={() => this.props.increaseCounter()}>
+                        <Text style={{ fontSize: 20 }}>+</Text>
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 20 }}>{this.props.counter}</Text>
+                    <TouchableOpacity onPress={() => this.props.decreaseCounter()}>
+                        <Text style={{ fontSize: 20 }}>-</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        counter: state.counter
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increaseCounter: () => dispatch({ type: 'INCREASE_COUNTER' }),
+        decreaseCounter: () => dispatch({ type: 'DECREASE_COUNTER' }),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterApp)
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  title: {
-    fontSize: 30,
-    color: "red",
-    paddingLeft: 10,
-    paddingRight: 10
-  },
-  value: {
-    fontSize: 30,
-    color: "#080808"
-  }
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
--- navigations/redux/SimpleReduxPage.tsx
+++ navigations/redux/SimpleReduxPage.tsx
@@ -1,32 +1,56 @@
-import React from "react";
-import { NavigationScreenProps } from "react-navigation";
-import { NavigationStackScreenOptions } from "react-navigation-stack/src/types";
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity
+} from "react-native";
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import AppCounter from "./AppCounter";
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-
-const reducer = () => {
-
+/**
+ * Store - holds our state - THERE IS ONLY ONE STATE 
+ * Action - State can be modified using actions - SIMPLE OBJECTS 
+ * Dispatcher - Action needs to be sent by someone - known as dispatching an action
+ * Reducer - receives the action and modifies the state to give us a new state 
+ *  - pure functions 
+ *  - only mandatory argument is the 'type' 
+ * Subscriber - listens for state change to update the ui  
+ */
+const initialState = {
+    counter: 0
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREASE_COUNTER':
+            return { counter: state.counter + 1 }
+        case 'DECREASE_COUNTER':
+            return { counter: state.counter - 1 }
+    }
+    return state
 }
 
 const store = createStore(reducer)
 
-export default class SimpleReduxPage extends React.Component {
-  public static navigationOptions = (
-    props: NavigationScreenProps
-  ): NavigationStackScreenOptions => {
-    return {
-      title: "redux简单实现"
-    };
-  };
- 
+class SimpleReduxPage extends Component {
 
-  public render() {
-    return (
-      <Provider store={store}>
-        <AppCounter />
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <AppCounter />
+            </Provider>
+        );
+    }
 }
 
+export default SimpleReduxPage
+
+// export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
